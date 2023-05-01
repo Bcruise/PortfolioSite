91648620a103ffc9b9c5373ca6106b4b761f5ac7
Ben Cruise <ben_cruise@hotmail.co.uk>
2023-05-01 19:40:49 +0100

    adding of Trade Wars and filter for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from './Header';
 import IndProject from "./IndProject";
 import headersInfo from '../data/headers.json';
 import work from '../data/work.json';
+import '../css/Projects.css';
+
 let projectCarrier = '';
 let workCarrier = work;
 
@@ -13,11 +15,20 @@ headersInfo.forEach(val => {
 });
 
 
-
 function Projects() {
+  
+  const techStackArray = [...new Set(work.map(obj => obj.techStack).flat())];
+  
   return (
     <>
       <Header title={projectCarrier.title} description={projectCarrier.description}/> 
+        <div className="select-div col-12 p-2">
+          <select className="col-11">
+            {techStackArray.map(tech => <>
+              <option>{tech}</option>
+            </>)}
+          </select>
+        </div>
       {workCarrier.map(project => <IndProject project={project}/>)};
     </>    
   );
